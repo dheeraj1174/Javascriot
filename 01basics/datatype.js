f0fdbce6de917ad9a99c66031d1ac345e9fa5883
dheeraj1174 <119004026+dheeraj1174@users.noreply.github.com>
2024-02-27 17:57:09 +0000

    conversion of data type

---
+++ 01basics/datatype.js
@@ -0,0 +1,28 @@
+"use strict";
+
+
+//alert(3+3)//we are using node js ,not browser
+
+
+
+let name = "hitesh";
+let age = 18
+let isLoggedIn = false
+let state;
+
+//number = 2 to power 53
+//bigint ko jb vo range se nikal jata hn tb use krte hn
+//string ""
+//boolean => true/false
+//null => standalone value
+//undefined means koi value h nhi defined kri
+//symbol => unique
+
+
+
+//object
+console.log(typeof "hitesh");
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
+
+
---
+++ 01basics/variable.js
@@ -0,0 +1,11 @@
+const accountId = 14453
+let accountEmail = "hitesh@gmail.com"
+var accountpass = "12345"
+accountcity = "jaipur"
+
+
+accountEmail = "hc.hc@gmail.com"
+accountpass = "12321"
+accountcity = "bengaluru"
+
+console.table([accountId,accountEmail,accountpass,accountcity]);
