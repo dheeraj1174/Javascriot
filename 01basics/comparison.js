e589d98d78d0d51b2cff36b294dbb69e86d7e95d
dheeraj1174 <119004026+dheeraj1174@users.noreply.github.com>
2024-02-28 14:19:56 +0000

    comparison

---
+++ 01basics/comparison.js
@@ -0,0 +1,24 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+console.log("2">1);
+console.log("02">1);
+//comparison operator convert null to a number treating it as 0 null convert to Nan also
+//that why null>=0 is true and null>0 is false
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+
+
+//=== this operator define basically check 2 condition first both datatype are equal and number is also same
+
+console.log("2"===2);
+
